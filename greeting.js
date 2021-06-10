fec43371cc3e75029a9068b9f0c3c5f614ee9215
kimsimo <62370411+kimsimo@users.noreply.github.com>
2021-06-10 13:28:44 +0900

    memorize name locally

---
+++ greeting.js
@@ -0,0 +1,30 @@
+const greeting = document.querySelector(".js-greeting"),
+  nameForm = document.querySelector(".nameForm"),
+  nameInput = nameForm.querySelector("input");
+
+function removenameInput() {
+  nameInput.classList.add("name-exist");
+}
+
+function loadName() {
+  const name = localStorage.getItem("name");
+  if (name !== null) {
+    removenameInput();
+    greeting.innerText = `Hello ${name}`;
+  }
+}
+
+function saveName(name) {
+  localStorage.setItem("name", name);
+}
+
+function handleSubmit(event) {
+  saveName(nameInput.value);
+}
+
+function init() {
+  loadName();
+  nameForm.addEventListener("submit", handleSubmit);
+}
+
+init();
